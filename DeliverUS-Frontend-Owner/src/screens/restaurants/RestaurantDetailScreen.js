3b4f55241e2db850ad970631405c528b02a9d0ba
noevilsan <noevilsan@gmail.com>
2026-04-15 09:48:30 +0200

    feat: lab 4 hecho (sin annex)

--- DeliverUS-Frontend-Owner/src/screens/restaurants/RestaurantDetailScreen.js
+++ DeliverUS-Frontend-Owner/src/screens/restaurants/RestaurantDetailScreen.js
@@ -11,13 +11,40 @@ import TextRegular from '../../components/TextRegular'
 import { API_BASE_URL } from '@env'
 
 export default function RestaurantDetailScreen({ route }) {
-  const { id } = route.params
+  const [restaurant, setRestaurant] = useState({})
+
+  useEffect(() => {
+    console.log('Loading restaurant details, please wait 1 second')
+    setTimeout(() => {
+      setRestaurant(getDetail(route.params.id))
+      console.log('Restaurant details loaded')
+    }, 1000)
+  }, [])
+
+  const renderProduct = ({ item }) => {
+    return (
+      <Pressable
+        style={styles.row}
+        onPress={() => { }}>
+          <TextRegular>
+              {item.name}
+          </TextRegular>
+      </Pressable>
+    )
+  }
+  
   return (
-    <View style={styles.container}>
-      <TextRegular style={{ fontSize: 16, alignSelf: 'center', margin: 20 }}>
-        Restaurant details. Id: {id}
-      </TextRegular>
-    </View>
+        <View style={styles.container}>
+            <TextRegular style={styles.textTitle}>{restaurant.name}</TextRegular>
+            <TextRegular style={styles.text}>{restaurant.description}</TextRegular>
+            <TextRegular style={styles.text}>shippingCosts: {restaurant.shippingCosts}</TextRegular>
+            <FlatList
+              style={styles.container}
+              data={restaurant.products}
+              renderItem={renderProduct}
+              keyExtractor={item => item.id.toString()}
+            />
+        </View>
   )
 }
 
--- DeliverUS-Frontend-Owner/src/screens/restaurants/RestaurantsScreen.js
+++ DeliverUS-Frontend-Owner/src/screens/restaurants/RestaurantsScreen.js
@@ -8,30 +8,38 @@ import restaurantLogo from '../../../assets/restaurantLogo.jpeg'
 import { API_BASE_URL } from '@env'
 
 export default function RestaurantsScreen({ navigation, route }) {
-  return (
-    <View style={styles.container}>
-      <TextRegular style={{ fontSize: 16, alignSelf: 'center', margin: 20 }}>
-        Random Restaurant
-      </TextRegular>
+  const [restaurants, setRestaurants] = useState([])
+
+  useEffect(() => {
+    console.log('Loading restaurants, please wait 2 seconds')
+    setTimeout(() => {
+      setRestaurants(getAll) // getAll function has to be imported
+      console.log('Restaurants loaded')
+    }, 2000)
+  }, [])
+
+  const renderRestaurant = ({ item }) => {
+    return (
       <Pressable
+        style={styles.row}
         onPress={() => {
-          navigation.navigate('RestaurantDetailScreen', {
-            id: Math.floor(Math.random() * 100)
-          })
-        }}
-        style={({ pressed }) => [
-          {
-            backgroundColor: pressed
-              ? GlobalStyles.brandBlueTap
-              : GlobalStyles.brandBlue
-          },
-          styles.actionButton
-        ]}
-      >
-        <TextRegular textStyle={styles.text}>Restaurant Details</TextRegular>
+          navigation.navigate('RestaurantDetailScreen', { id: item.id })
+        }}>
+          <TextRegular>
+              {item.name}
+          </TextRegular>
       </Pressable>
-    </View>
-  )
+    )
+  }
+
+  return (
+        <FlatList
+          style={styles.container}
+          data={restaurants}
+          renderItem={renderRestaurant}
+          keyExtractor={item => item.id.toString()}
+        />
+    )
 }
 
 const styles = StyleSheet.create({
